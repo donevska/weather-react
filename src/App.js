1f4d3cfd7c055c95ccd7df8540731e9eee41d2df
donevska <angeladonevska@gmail.com>
2022-02-06 02:19:01 +0100

    Forecast API integrated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
-import Footer from "./Footer";
 
 import "./App.css";
 
@@ -10,7 +9,24 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Skopje" />
-        <Footer />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/angela-donevska"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Angela Donevska
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/donevska/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github.
+          </a>
+        </footer>
       </div>
     </div>
   );
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,41 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+  return (
+    <div>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{day()}</h5>
+          <p>
+            <span className="card-text min-temperature">
+              {minTemperature()}
+            </span>
+            /<span className="max-temperature">{maxTemperature()}</span>
+          </p>
+          <WeatherIcon
+            className="forecast-image"
+            code={props.data.weather[0].icon}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
